3fa66f7fecfd3a70551a566a4878810389539c73
99003754 <78854545+99003754@users.noreply.github.com>
2021-03-15 14:38:41 +0530

    Update color.h

--- color_class&object/color.h
+++ color_class&object/color.h
@@ -4,15 +4,15 @@
 #include<math.h>
 using namespace std;
 #include<string.h>
-class color{
+class colorr{
 public:
 int m_r;
 int m_g;
 int m_b;
-color();
-color(int red,int green,int blue);
-color(std::string str);
-color(int col[3]);
+colorr();
+colorr(int ,int ,int );
+colorr(std::string str);
+colorr(int col[3]);
 void invert();
 int display();
 };
